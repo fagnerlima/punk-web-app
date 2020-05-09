46d91a1a27ded757f2649ca5e67b0f9ea63d2c1c
Fagner Lima <contato@fagnerlima.pro.br>
2020-05-09 15:24:46 -0300

    Adiciona informações em BeerDetails

---
+++ src/pages/beer/beer-details/index.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+
+import './styles.css';
+import http from '../../../services/http';
+import HeaderPage from '../../../components/HeaderPage';
+import FooterPage from '../../../components/FooterPage';
+import Button from '../../../components/Button';
+
+class BeerDetails extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      beer: {}
+    };
+  }
+
+  componentDidMount() {
+    this.loadBeer();
+  }
+
+  loadBeer = async () => {
+    try {
+      const { id } = this.props.match.params;
+      const response = await http.get(`/beers/${id}`);
+      this.setState({
+        beer: response.data[0]
+      });
+    } catch (error) {
+      console.error('loadProduct error', error);
+    }
+  }
+
+  render() {
+    const { beer } = this.state;
+
+    return (
+      <>
+        <HeaderPage>Beer Details</HeaderPage>
+        <div className="beer-details">
+          <div className="beer-image">
+            <img src={beer.image_url} alt={beer.name} title={beer.name} />
+          </div>
+          <div className="beer-info">
+            <h4 className="beer-name">{beer.name}</h4>
+            <p><b>Tagline</b>: {beer.tagline}</p>
+            <p><b>First Brewed</b>: {beer.first_brewed}</p>
+            <p><b>Description</b>: {beer.description}</p>
+            <p><b>Ingredients</b>:</p>
+            <ul className="beer-ingredients">
+              <li>
+                <b>Malt</b>
+                <ul>
+                  {beer.ingredients?.malt?.map(malt => (
+                    <li>{malt.name} - {malt.amount.value} {malt.amount.unit}</li>
+                  ))}
+                </ul>
+              </li>
+              <li>
+                <b>Hoops</b>
+                <ul>
+                  {beer.ingredients?.hops?.map(hop => (
+                    <li>{hop.name} - {hop.amount.value} {hop.amount.unit}</li>
+                  ))}
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <FooterPage>
+          <Button
+            type="link"
+            color="primary"
+            to="/beers"
+          >
+            Voltar
+          </Button>
+        </FooterPage>
+      </>
+    );
+  }
+}
+
+export default BeerDetails;
--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 import BeerList from './pages/beer/beer-list';
-import BeerDetail from './pages/beer/beer-detail';
+import BeerDetails from './pages/beer/beer-details';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/beers" component={BeerList} />
-      <Route exact path="/beers/:id" component={BeerDetail} />
+      <Route exact path="/beers/:id" component={BeerDetails} />
       <Redirect from="*" to="/beers" />
     </Switch>
   </BrowserRouter>

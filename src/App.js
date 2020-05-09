93cb8dffb9915b7a408044869db8a02d83913b4e
Fagner Lima <contato@fagnerlima.pro.br>
2020-05-09 14:25:18 -0300

    Implementa componente Container

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import './App.css';
 import Routes from './routes';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Container from './components/Container';
 
 const App = () => (
   <div className="App">
     <Header />
-    <div className="container">
+    <Container>
       <Routes />
-    </div>
+    </Container>
     <Footer />
   </div>
 );

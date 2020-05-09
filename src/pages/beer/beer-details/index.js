ec70691bd90fa60897c5f4e751f59c0085e39641
Fagner Lima <contato@fagnerlima.pro.br>
2020-05-09 15:53:26 -0300

    Adiciona Load em BeerDetails

--- src/pages/beer/beer-details/index.js
+++ src/pages/beer/beer-details/index.js
@@ -5,13 +5,16 @@ import http from '../../../services/http';
 import HeaderPage from '../../../components/HeaderPage';
 import FooterPage from '../../../components/FooterPage';
 import Button from '../../../components/Button';
+import Load from '../../../components/Load';
+import Alert from '../../../components/Alert';
 
 class BeerDetails extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      beer: {}
+      beer: null,
+      isLoaded: false
     };
   }
 
@@ -28,57 +31,69 @@ class BeerDetails extends Component {
       });
     } catch (error) {
       console.error('loadProduct error', error);
+    } finally {
+      this.setState({
+        isLoaded: true
+      });
     }
   }
 
   render() {
-    const { beer } = this.state;
+    const { isLoaded } = this.state;
 
     return (
       <>
         <HeaderPage>Beer Details</HeaderPage>
-        <div className="beer-details">
-          <div className="beer-image">
-            <img src={beer.image_url} alt={beer.name} title={beer.name} />
-          </div>
-          <div className="beer-info">
-            <h4 className="beer-name">{beer.name}</h4>
-            <p><b>Tagline</b>: {beer.tagline}</p>
-            <p><b>First Brewed</b>: {beer.first_brewed}</p>
-            <p><b>Description</b>: {beer.description}</p>
-            <p><b>Ingredients</b>:</p>
-            <ul className="beer-ingredients">
-              <li>
-                <b>Malt:</b>
-                <ul>
-                  {beer.ingredients?.malt?.map(malt => (
-                    <li>{malt.name} - {malt.amount.value} {malt.amount.unit}</li>
-                  ))}
-                </ul>
-              </li>
-              <li>
-                <b>Hoops:</b>
-                <ul>
-                  {beer.ingredients?.hops?.map(hop => (
-                    <li>{hop.name} - {hop.amount.value} {hop.amount.unit}</li>
-                  ))}
-                </ul>
-              </li>
-            </ul>
-          </div>
-        </div>
+        {!isLoaded ? <Load /> : this.beerDetails()}
         <FooterPage>
-          <Button
-            type="link"
-            color="primary"
-            to="/beers"
-          >
+          <Button type="link" color="primary"to="/beers">
             Voltar
           </Button>
         </FooterPage>
       </>
     );
   }
+
+  beerDetails = () => {
+    const { beer } = this.state;
+
+    if (!beer) {
+      return <Alert type="danger">Information not found</Alert>;
+    }
+
+    return (
+      <div className="beer-details">
+        <div className="beer-image">
+          <img src={beer.image_url} alt={beer.name} title={beer.name} />
+        </div>
+        <div className="beer-info">
+          <h4 className="beer-name">{beer.name}</h4>
+          <p><b>Tagline</b>: {beer.tagline}</p>
+          <p><b>First Brewed</b>: {beer.first_brewed}</p>
+          <p><b>Description</b>: {beer.description}</p>
+          <p><b>Ingredients</b>:</p>
+          <ul className="beer-ingredients">
+            <li>
+              <b>Malt:</b>
+              <ul>
+                {beer.ingredients?.malt?.map(malt => (
+                  <li>{malt.name} - {malt.amount.value} {malt.amount.unit}</li>
+                ))}
+              </ul>
+            </li>
+            <li>
+              <b>Hoops:</b>
+              <ul>
+                {beer.ingredients?.hops?.map(hop => (
+                  <li>{hop.name} - {hop.amount.value} {hop.amount.unit}</li>
+                ))}
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  };
 }
 
 export default BeerDetails;

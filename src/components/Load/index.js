02a62c51752e04fef3205d5d7b8aa3b9b3850a4c
Fagner Lima <contato@fagnerlima.pro.br>
2020-05-09 15:53:14 -0300

    Implementa componente Load

---
+++ src/components/Load/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+
+import './styles.css';
+
+const Load = () => (
+  <div className="load">
+    <FontAwesomeIcon icon={faSpinner} spin />
+  </div>
+);
+
+export default Load;
